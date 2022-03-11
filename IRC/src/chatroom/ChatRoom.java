14710500c6d32b3d52854aef568ffcc050408d22
parasharparth <parthparashar6@gmail.com>
2022-03-11 03:41:18 -0800

    restoring version-1 and improvement in version-2

--- IRC/src/chatroom/ChatRoom.java
+++ IRC/src/chatroom/ChatRoom.java
@@ -7,7 +7,7 @@ public class ChatRoom
     public String name;
     
     public static HashSet<String> clients = new HashSet<String>(); // Hashset to keep a record of all unique clients so that duplicate clients are not created
-     
+      
     public static HashSet<PrintWriter> writers = new HashSet<PrintWriter>(); // Hashset to keep a record of all writers for unique clients for easy broadcast in chatroom.
     
     public void addParticipant(String clientName) 
